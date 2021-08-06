69bba727a008e625f1e9e54890c4a111a6a80cf4
Kha Tran <khadieptran@gmail.com>
2021-08-05 22:10:47 -0400

    Implement rainbowMode by randomize color

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Set the color variable
 let color = '#000000';
+let isColorRandom = false;
 
 // Create 16x16 pixel squares on the for the sketch pad
 const sketchPad = document.getElementById("sketch-pad");
@@ -16,6 +17,9 @@ for (let i = 0; i < 16**2; i++) {
 
 // Listen for mouse over a square and change its color
 sketchPad.addEventListener('mouseover', function(event) {
+    if (isColorRandom) {
+        color = randomizeColor();
+    }
     event.target.style['background-color'] = color;
 })
 
@@ -26,3 +30,17 @@ colorPicker.addEventListener("change", function(event) {
     color = event.target.value;
 })
 
+// Listen for rainbow mode
+const rainbow = document.getElementById("rainbow");
+rainbow.addEventListener('click', rainbowColor)
+
+function rainbowColor() {
+    isColorRandom = true;
+}
+
+function randomizeColor() {
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+    return `rgb(${r}, ${g}, ${b})`;
+}
