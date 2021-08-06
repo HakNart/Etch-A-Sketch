7dc825bc49f1d21b10e4889faa970f207fa8b6b2
Kha Tran <khadieptran@gmail.com>
2021-08-05 21:15:19 -0400

    Implement color picker for standard mode

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// Set the color variable
+let color = '#000000';
+
 // Create 16x16 pixel squares on the for the sketch pad
 const sketchPad = document.getElementById("sketch-pad");
 
@@ -13,5 +16,13 @@ for (let i = 0; i < 16**2; i++) {
 
 // Listen for mouse over a square and change its color
 sketchPad.addEventListener('mouseover', function(event) {
-    event.target.style['background-color'] = "black";
+    event.target.style['background-color'] = color;
 })
+
+// Listen for color change
+const colorPicker = document.getElementById("color-picker");
+console.log(colorPicker);
+colorPicker.addEventListener("change", function(event) {
+    color = event.target.value;
+})
+
